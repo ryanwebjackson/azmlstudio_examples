a643a2d285809f17422c1c311804c08f2fa8539f
Ryan Jackson <ryanjackson0y-@live.com>
2023-07-26 09:10:52 -0500

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+# Download Python and IPython notebook
+# curl http://continuum.io/downloads 
+
+# Open IPython Notebook
+open http://localhost:8888/tree
+
+# Set Python path
+path=%path%;/python34/scripts
+
+# Set up Azure tools
+pip install azureml
+
